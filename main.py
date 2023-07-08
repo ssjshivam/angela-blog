275d9da92d099029e930c9f21b1a3d23ac81c3fa
Surya Pratap Singh <ssjshivam2777@gmail.com>
2023-07-08 17:12:13 +0530

    Added secret key to environment variable

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from forms import CreatePostForm, CreateUser, LogInUser, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
